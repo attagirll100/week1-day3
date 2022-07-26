e703b195ade68b49c33d750437262fa2b957f2a8
Mary <attamary062@gmail.com>
2022-07-26 17:36:02 -0400

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,21 @@
+for (let i = 0; i < 99; i++) {
+    console.log("Hello World");
+}
+
+for (let i = 99; i >= 0; i++) {
+    console.log("Great");
+}
+
+for (let i = 49; i <=72; i++) {
+    console.log("Awesome", i);
+}
+    
+for (let i = 0; i <= 99; i++) {
+    if (i % 2 !== 0) console.log(i);
+}
+
+for (let i = 81; i>=26; i--) {
+    console.log("Okay");
+
+
+}
